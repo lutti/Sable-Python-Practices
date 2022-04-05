d6dbdeab79e08a5b3ebce0618385ec332bd70748
Lutti <lutti.t.amaral@gmail.com>
2022-04-04 23:42:42 -0300

    more intuitive form to perform the algoritm
    just figured it out to be more intuitive and simple code adjusting arrayToIterate to be always uppercase

--- Cracking-rot13.py
+++ Cracking-rot13.py
@@ -3,14 +3,14 @@ arrayToIterate = "jjj.cnfgrova.pbz"
 arrayOutput = ""
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
-alphabet2 = alphabet2.lower()
+alphabet2 = alphabet2
 numOffset = 13
 numb = 0
 
-for ch in arrayToIterate.lower():
+for ch in arrayToIterate.upper():
     numb = 0
     if ch != " ":
-        for alphach in alphabet.lower():
+        for alphach in alphabet:
             
             if ch == alphach:
                 arrayOutput += alphabet2[numOffset + numb:numOffset + numb + 1]
